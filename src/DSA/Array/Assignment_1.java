179313ac2983ad7f925773e00763aae2c31140d0
anii-123 <bootcoding@gmail.com>
2023-10-09 22:13:39 +0530

    program

---
+++ src/DSA/Array/Assignment_1.java
@@ -0,0 +1,22 @@
+package Array;
+
+import java.util.Scanner;
+
+public class Assignment_1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number of element that you want to store ");
+        int n = s.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter number of element");
+        for (int i = 0; i <n; i++) {
+            arr[i] = s.nextInt();
+        }
+        System.out.println("Array element are : ");
+        for (int i = 0; i < n; i++) {
+            System.out.print( arr[i] + "  ");
+        }
+
+    }
+}
+
